edaf477cbd78c7420156387e045160c011280619
Bishalpoddar06 <poddarbishal1994@gmail.com>
2025-09-03 09:59:25 +0530

    Hello

---
+++ DSA/Assignment005/program1.c
@@ -0,0 +1,8 @@
+/*
+write a program in C to perform the following stack operations with a menu based system. Observe the fow of data in stack and write the outputs in your laboratory notes.
+i. push- add item to stack.(print overflow if stack is full)
+ii. pop- remove item from stack
+iii. peek - see the top item of stack
+iv. view - gives the view of the stack
+Instruction:- 1. You may use a global integer for the stack.
+*/
